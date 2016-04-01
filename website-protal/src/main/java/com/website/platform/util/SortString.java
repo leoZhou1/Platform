2d761aa6792f4f5328961c6f1d0ae888b86d953f
leoZhou1 <530186424@qq.com>
2016-04-01 18:21:43 +0800

    添加代码

---
+++ website-protal/src/main/java/com/website/platform/util/SortString.java
@@ -0,0 +1,18 @@
+package com.website.platform.util;
+
+//变位词排序：
+//请编写一个方法，对一个字符串数组进行排序，将所有变位词合并，保留其字典序最小的一个串。
+//这里的变位词指变换其字母顺序所构成的新的词或短语。例如"triangle"和"integral"就是变位词。
+//给定一个string的数组str和数组大小int n，请返回排序合并后的数组。保证字符串串长小于等于20，数组大小小于等于300。
+//测试样例：
+//["ab","ba","abc","cba"]
+//返回：["ab","abc"]
+import java.util.*;
+
+public class SortString {
+    public ArrayList<String> sortStrings(String[] str, int n) {
+        // 在这里写你的代码
+    	
+    	return null;
+    }
+}
