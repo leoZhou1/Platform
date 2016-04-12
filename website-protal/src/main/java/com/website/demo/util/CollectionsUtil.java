2b9a77b846f037a73bb1d610391434ac5616bc5e
leoZhou1 <530186424@qq.com>
2016-04-12 22:35:47 +0800

    添加代码

---
+++ website-protal/src/main/java/com/website/demo/util/CollectionsUtil.java
@@ -0,0 +1,13 @@
+package com.website.demo.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionsUtil {
+	
+	
+	private static final List<String> numList = new ArrayList<String>(
+			Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "一", "二", "三", "四", "五", "六", "七", "八", "九"));
+
+}
