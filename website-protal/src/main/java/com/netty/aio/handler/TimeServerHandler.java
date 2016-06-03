ccc46555af8715af03295466ea64734d66a47206
leoZhou1 <530186424@qq.com>
2016-06-03 18:04:48 +0800

    添加代码

--- website-protal/src/main/java/com/netty/aio/handler/TimeServerHandler.java
+++ website-protal/src/main/java/com/netty/aio/handler/TimeServerHandler.java
@@ -7,24 +7,27 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
-public class TimeServerHandler extends ChannelHandlerAdapter{
+public class TimeServerHandler extends ChannelHandlerAdapter {
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		ByteBuf buf = (ByteBuf) msg;
 		byte[] req = new byte[buf.readableBytes()];
 		buf.readBytes(req);
 		String body = new String(req, "UTF-8");
-		System.out.println("body:  "+body);
-		String currentTime = "QUERY_TIME_ORDER".equals(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+		System.out.println("body:  " + body);
+		String currentTime = "QUERY_TIME_ORDER".equals(body) ? 
+				new Date(System.currentTimeMillis()).toString()
+				: "BAD ORDER";
 		ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+//		resp.writeBytes(resp);
 		ctx.write(resp);
 	}
-	
+
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
 		ctx.flush();
 	}
-	
+
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 		ctx.close();
